d1a86a7a218569415c3cf9a100ad93d2a0435d43
Thomas Guyader <thomas.guyader@recif.nc>
2019-02-19 13:26:49 +1100

    chore(adf): utilisation de adf essentials client

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-unzip -o adf-essentials.zip
+unzip -o adf-essentials-client-ear.zip
 
 touch dependencies.pom
 
@@ -8,7 +8,7 @@ find ./modules -name "*.jar"|while read fullname; do
   jarname=`basename $fullname .jar`
   echo "jar name : $jarname"
 
-  ./mvnw -s ./settingsWithLocalMirror.xml install:install-file -Dfile=$fullname -DgroupId=oracle.essentials -DartifactId=$jarname -Dversion=12.2.1.3.0 -Dpackaging=oracle.essentials -DgeneratePom=true
+  ./mvnw -s ./settingsWithLocalMirror.xml deploy:deploy-file -Dfile=$fullname -DgroupId=oracle.essentials -DartifactId=$jarname -Dversion=12.2.1.3.0 -Dpackaging=oracle.essentials -DgeneratePom=true -DrepositoryId=enterprise-repo -Durl=http://localhost:8081/artifactory/virtual-maven-repositories
   echo "<dependency><groupId>oracle.essentials</groupId><artifactId>$jarname</artifactId><version>\${dependencies.version.adf.essentials}</version></dependency>" >> dependencies.pom
 
 done
